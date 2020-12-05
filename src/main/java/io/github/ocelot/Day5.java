dae18d6db2bdc110dba9e84e8b5a0f222d231580
Ocelot <FinntheRaider@gmail.com>
2020-12-04 21:47:30 -0800

    Completed day 5

---
+++ src/main/java/io/github/ocelot/Day5.java
@@ -0,0 +1,83 @@
+package io.github.ocelot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Ocelot
+ */
+public class Day5
+{
+    public static void main(String[] args) throws IOException
+    {
+        // Parse input data
+        StringBuilder data = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(Day5.class.getResourceAsStream("/5/input.txt"))))
+        {
+            while (reader.ready())
+                data.append(reader.readLine().trim()).append(",");
+        }
+        String[] input = Arrays.stream(data.toString().split(",")).toArray(String[]::new);
+
+        // Process each line as a boarding pass
+        int[] result = new int[input.length];
+        for (int i = 0; i < input.length; i++)
+        {
+            String pass = input[i];
+            if (pass.length() != 10) // This should never be thrown
+                throw new IllegalArgumentException("Invalid boarding pass '" + pass + "'");
+
+            int row = parse(pass.substring(0, 7)); // Parse row from first 7 chars
+            int col = parse(pass.substring(7, 10)); // Parse col from last 3 chars
+
+            result[i] = row * 8 + col; // Generate id from row and col
+        }
+
+        // Print highest seat id
+        Arrays.stream(result).max().ifPresent(max -> System.out.println("Highest seat id is " + max));
+
+        // Create a set of all adjacent seat ids
+        Set<Integer> values = new HashSet<>();
+        Arrays.stream(result).forEach(value ->
+        {
+            values.add(value - 1);
+            values.add(value + 1);
+        });
+        // Remove all seat ids in the set
+        values.removeIf(value -> Arrays.stream(result).anyMatch(s -> value == s));
+
+        // Print second result
+        System.out.println("Missing seats are " + values);
+    }
+
+    /**
+     * Parses a binary tree from the input data.
+     *
+     * @param input The input data to parse
+     * @return A single number that was parsed from the binary operation
+     */
+    private static int parse(String input)
+    {
+        // Generate initial range from the input data
+        IntRange range = new IntRange(0, (int) Math.pow(2, input.length()) - 1);
+
+        // Loop through input to split range into upper and lower until a single number is found
+        for (int i = 0; i < input.length(); i++)
+        {
+            double median = range.getMedian();
+            switch (input.charAt(i))
+            {
+                case 'F', 'L' -> range = new IntRange(range.getMin(), (int) Math.floor(median)); // Lower half
+                case 'B', 'R' -> range = new IntRange((int) Math.ceil(median), range.getMax()); // Upper half
+                default -> throw new IllegalStateException("Invalid segment '" + input.charAt(i) + "'"); // This should never be thrown
+            }
+        }
+        if (range.getMin() != range.getMax()) // This should never be thrown
+            throw new IllegalStateException("Range did not become a single number. Was " + range);
+        return range.getMin(); // min is the same as max so it doesn't matter which is returned
+    }
+}
--- src/main/java/io/github/ocelot/IntRange.java
+++ src/main/java/io/github/ocelot/IntRange.java
@@ -1,7 +1,13 @@
 package io.github.ocelot;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 
+/**
+ * Denotes a range of integer values.
+ *
+ * @author Ocelot
+ */
 public class IntRange implements Predicate<Integer>
 {
     private final int min;
@@ -10,11 +16,17 @@ public class IntRange implements Predicate<Integer>
     public IntRange(int min, int max)
     {
         if (min > max)
-            throw new IllegalArgumentException("Min must be less than or equal to max");
+            throw new IllegalArgumentException("Min (" + min + ") must be less than or equal to max (" + max + ")");
         this.min = min;
         this.max = max;
     }
 
+    /**
+     * Parses an int range from a string sequence of <code>#-#</code>
+     *
+     * @param input The input string to parse
+     * @return A new integer range from the input data
+     */
     public static IntRange parse(String input)
     {
         String[] parts = input.split("-");
@@ -27,5 +39,44 @@ public class IntRange implements Predicate<Integer>
     public boolean test(Integer integer)
     {
         return integer >= this.min && integer <= this.max;
+    }
+
+    public double getMedian()
+    {
+        return this.min + (this.max - this.min) / 2.0;
+    }
+
+    public int getMin()
+    {
+        return min;
+    }
+
+    public int getMax()
+    {
+        return max;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IntRange intRange = (IntRange) o;
+        return min == intRange.min && max == intRange.max;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(min, max);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "IntRange{" +
+                "min=" + min +
+                ", max=" + max +
+                '}';
     }
 }
